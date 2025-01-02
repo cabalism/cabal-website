8b0940f55b343640f9988fa4b89603a2fd11bf01
Phil de Joux <philderbeast@gmail.com>
2025-01-02 16:14:22 -0500

    Satisfy fix-whitespace

--- color-modes.js
+++ color-modes.js
@@ -6,19 +6,19 @@
 
 (() => {
     'use strict'
-  
+
     const getStoredTheme = () => localStorage.getItem('theme')
     const setStoredTheme = theme => localStorage.setItem('theme', theme)
-  
+
     const getPreferredTheme = () => {
       const storedTheme = getStoredTheme()
       if (storedTheme) {
         return storedTheme
       }
-  
+
       return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     }
-  
+
     const setTheme = theme => {
       if (theme === 'auto') {
         document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
@@ -26,29 +26,29 @@
         document.documentElement.setAttribute('data-bs-theme', theme)
       }
     }
-  
+
     setTheme(getPreferredTheme())
-  
+
     const showActiveTheme = (theme, focus = false) => {
       const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-  
+
       document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
         element.classList.remove('active')
       })
 
       btnToActive.classList.add('active')
     }
-  
+
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
       const storedTheme = getStoredTheme()
       if (storedTheme !== 'light' && storedTheme !== 'dark') {
         setTheme(getPreferredTheme())
       }
     })
-  
+
     window.addEventListener('DOMContentLoaded', () => {
       showActiveTheme(getPreferredTheme())
-  
+
       document.querySelectorAll('[data-bs-theme-value]')
         .forEach(toggle => {
           toggle.addEventListener('click', () => {
